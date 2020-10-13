d08ca06f4b8b42ec520c4534d853aa51e8ced305
enrico <enricofer@gmail.com>
2020-10-13 17:42:41 +0200

    old style urls

--- version/urls.py
+++ version/urls.py
@@ -30,8 +30,11 @@ urlpatterns = [
     url(r'^save/$', views.save_restricted.as_view(), name='save'),
     url(r'^tree/(\d+)?/$', views.vtree_restricted.as_view(), name='vtree'),
     url(r'^list/(\d+)?/$', views.vlist_restricted.as_view(), name='vlist'),
-    path("token-auth/", jwt_auth_views.jwt_token),
-    path("token-refresh/", jwt_auth_views.refresh_jwt_token)
+    url(r'^token-auth/$',jwt_auth_views.jwt_token),
+    url(r'^token-refresh/$',jwt_auth_views.refresh_jwt_token),
+
+    #path("token-auth/", jwt_auth_views.jwt_token),
+    #path("token-refresh/", jwt_auth_views.refresh_jwt_token)
     #url(r'^check/(\d+)/$', views.checkAndMerge, name='checkVersion'),
 ]
 
