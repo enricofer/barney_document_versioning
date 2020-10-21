10727f071a06153753a379fd7025c8b95ca9c0f0
Enrico Ferreguti <enricofer@gmail.com>
2020-10-21 17:54:15 +0200

    test data

--- version/tests.py
+++ version/tests.py
@@ -1,11 +1,16 @@
 from django.test import TestCase
+from .models import Version
 
 # Create your tests here.
 class impianto(TestCase):
     @classmethod
     def setUpTestData(cls):
         print("setUpTestData: Run once to set up non-modified data for all class methods.")
-        pass
+        cls.d1 = Version.objects.create(
+            title="Test1"
+            base = ""
+            content = "content1"
+        )
 
     def setUp(self):
         print("setUp: Run once for every test method to setup clean data.")
