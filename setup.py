965524f8260f1a6d0aa41d6a2a765344dfe00d3d
enrico <enricofer@gmail.com>
2020-10-23 10:29:43 +0200

    add template.odt to pypi setup

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='barney-version',
-    version='0.1.15',
+    version='0.1.16',
     packages=find_packages(),
     install_requires=[
         'Django==1.11.20',
@@ -27,20 +27,23 @@ setup(
     ],
     include_package_data=True,
     license='GNU General Public License v3 (GPLv3)',  # example license
-    description='A Django app for easy/collaborative versioning of legal documents.',
-    url='https://github.com/enricofer/simple_md_versioning',
+    description='A Django app for easy/collaborative versioning of markdown legal documents.',
+    url='https://github.com/enricofer/barney_document_versioning',
     author='Enrico Ferreguti',
     author_email='enricofer@gmail.com',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 2.0',  # replace "X.Y" as appropriate
+        'Framework :: Django :: 2.2.16',  
+        'Framework :: Django :: 3.0.10',   
+        'Framework :: Django :: 3.1.2', 
         'Intended Audience :: Other Audience',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',  # example license
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
