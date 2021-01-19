35e21151ca2a60e9edc2b37b6fdbb9c57cb05eee
ferregutie <ferregutie@comune.padova.it>
2021-01-19 16:10:37 +0100

    bump release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='barney-version',
-    version='0.3.4',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'Django',
